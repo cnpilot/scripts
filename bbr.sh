f2ffe548654e266c3c570fc1010d3b196ed1bb09
cnpilot <32807915+cnpilot@users.noreply.github.com>
2023-11-02 10:23:58 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+export PATHl
 
 #=================================================
 #	System Required: Debian/Ubuntu
